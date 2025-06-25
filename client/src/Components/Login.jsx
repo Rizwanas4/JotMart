75ca0291cd110bdeadc80caab4800b148f542b13
Rizwanas4 <124036925+Rizwanas4@users.noreply.github.com>
2025-06-25 23:15:27 +0530

    Update Login.jsx

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
   e.preventDefault();
 
   try {
-    const response = await axios.post("http://localhost:5000/api/login", {
+    const response = await axios.post("https://jotmart-backend.onrender.com/login", {
       email,
       password
     });
